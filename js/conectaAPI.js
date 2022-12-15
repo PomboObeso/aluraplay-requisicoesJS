edd67d8d9634cc309bc7b2b778c64ec2953ada34
PomboObeso <matheus.gomes201@gmail.com>
2022-12-15 11:38:13 -0300

    initial commit

---
+++ js/conectaAPI.js
@@ -0,0 +1,13 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+    
+    console.log(conexaoConvertida);
+
+    return conexaoConvertida;
+}
+
+
+export const conectaAPI = {
+    listaVideos
+}
